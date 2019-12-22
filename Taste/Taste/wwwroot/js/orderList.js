252628696ffc9909db96fe99ab11b53cd4761300
furkancmz <44070906+furkancmz@users.noreply.github.com>
2019-12-22 22:29:39 +0300

    Dosyalar Yüklendi

---
+++ Taste/Taste/wwwroot/js/orderList.js
@@ -0,0 +1,47 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("cancelled")) {
+        loadList("cancelled");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadList("completed");
+        }
+        else {
+            loadList("");
+        }
+    }
+});
+
+function loadList(param) {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/order?status=" + param,
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "orderHeader.id", "width": "20%" },
+            { "data": "orderHeader.pickupName", "width": "20%" },
+            { "data": "orderHeader.applicationUser.email", "width": "20%" },
+            { "data": "orderHeader.orderTotal", "width": "20%" },
+            {
+                "data": "orderHeader.id",
+                "render": function (data) {
+                    return ` <div class="text-center">
+                                <a href="/Admin/Order/OrderDetails?id=${data}" class="btn btn-success text-white" style="cussor:pointer; width:100px;">
+                                    <i class="far fa-edit"></i> Details
+                                </a>
+                               
+                    </div>`;
+                }, "width": "20%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
